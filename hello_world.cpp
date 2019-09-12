b8d5a63f2a0e988a179d94dd576562d23fd3993a
Ubuntu <ubuntu@ip-172-31-28-83.ec2.internal>
2019-09-12 17:29:15 +0000

    created proper hello world program CPP

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2019 DJ Watson All Rights Reserved
+//
+// Created by DJ Watson
+// Created on September 2019
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
